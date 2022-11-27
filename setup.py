4f5c7ab8a0b40bdf9e893b5768c62584b72af212
dimitrys <dimitrys@rafael.co.il>
2022-11-27 09:36:42 +0200

    feat(automation): init project adding functionality

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="automation-locust-framework",
+    author="Dimitry",
+    description="Map colonies performance infrastructure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MapColonies/automation-locust-framework.git",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
