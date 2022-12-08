8084a7310a3a094a424b8bd0eeeedc255ec1d649
shay-perpi <110040106+shay-perpi@users.noreply.github.com>
2022-12-08 09:37:40 +0200

    Add files via upload
    Vector_query_service_sizing_locust

---
+++ tests/config.py
@@ -0,0 +1,14 @@
+version = "2.0.0"
+featureType = 'buildings'
+versions = ("1.0.0", "1.1.0", "2.0.0")
+
+
+exceptions = ("text/xml", "application/json", "text/javascript")
+
+outputFormat = ("application/xml", "application/json")
+
+
+by_date_filter = ('&count=', '&sortBy=date&count=')
+
+bbox_x = [29.73756916597569, 33.35012435913086]
+bbox_y = [ 34.21269226074219,  35.87874221801758]
