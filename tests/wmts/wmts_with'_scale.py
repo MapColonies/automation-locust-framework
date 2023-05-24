b7d515f3647122c6adac03e82ee1224e049be60c
shay-perpi <shayperp@rafael.co.il>
2023-05-24 11:16:10 +0300

    last version for 24 may

--- tests/wmts/wmts_with'_scale.py
+++ tests/wmts/wmts_with'_scale.py
@@ -6,7 +6,6 @@ from locust_plugins.csvreader import CSVReader
 from pathlib import Path
 from utils.percentile_calculation import calculate_times, generate_name
 
-
 myDir = os.getcwd()
 sys.path.append(myDir)
 files = os.listdir(myDir)
@@ -23,31 +22,32 @@ upscale_reader = CSVReader(wmts_csv_path_up_scale)
 
 file_name = generate_name(__name__)
 stat_file = open(f"{config_obj['wmts'].root_dir}/{file_name}", 'w')
+
+
 class User(FastHttpUser):
     between(1, 1)
 
     @task(1)  # #WMTS - “HTTP_REQUEST_TYPE /SUB_DOMAIN/PROTOCOL/LAYER/TILE_MATRIX_SET/Z/X/Y.IMAGE_FORMAT HTTP_VERSION“
     @tag("wmts-loading")
     def index(self):
-        if config_obj['wmts'].WMTS_FLAG:
-            points = next(ssn_reader)
-            if config_obj["wmts"].TOKEN is None:
-                self.client.get(
-                    f"/{config_obj['wmts'].LAYER_TYPE}/"
-                    f"{config_obj['wmts'].LAYER_NAME}/"
-                    f"{config_obj['wmts'].GRID_NAME}/"
-                    f"{points[0]}/{points[1]}/{points[2]}"
-                    f"{config_obj['wmts'].IMAGE_FORMAT}",
-                )
-            else:
-                self.client.get(
-                    f"/{config_obj['wmts'].LAYER_TYPE}/"
-                    f"{config_obj['wmts'].LAYER_NAME}/"
-                    f"{config_obj['wmts'].GRID_NAME}/"
-                    f"{points[0]}/{points[1]}/{points[2]}"
-                    f"{config_obj['wmts'].IMAGE_FORMAT}"
-                    f"?token={config_obj['wmts'].TOKEN}",
-                )
+        points = next(ssn_reader)
+        if config_obj["wmts"].TOKEN is None:
+            self.client.get(
+                f"/{config_obj['wmts'].LAYER_TYPE}/"
+                f"{config_obj['wmts'].LAYER_NAME}/"
+                f"{config_obj['wmts'].GRID_NAME}/"
+                f"{points[0]}/{points[1]}/{points[2]}"
+                f"{config_obj['wmts'].IMAGE_FORMAT}",
+            )
+        else:
+            self.client.get(
+                f"/{config_obj['wmts'].LAYER_TYPE}/"
+                f"{config_obj['wmts'].LAYER_NAME}/"
+                f"{config_obj['wmts'].GRID_NAME}/"
+                f"{points[0]}/{points[1]}/{points[2]}"
+                f"{config_obj['wmts'].IMAGE_FORMAT}"
+                f"?token={config_obj['wmts'].TOKEN}",
+            )
 
     @task(1)
     @tag("Wmts-Upscale")
@@ -72,9 +72,6 @@ class User(FastHttpUser):
             )
 
     host = 'https://mapproxy-no-auth-raster-qa.apps.j1lk3njp.eastus.aroapp.io/api/raster/v1'
-
-    # host = config_obj["wmts"].HOST
-    # host = "http://lb-mapcolonies.gg.wwest.local/mapproxy-ww"
 
     def on_stop(self):
         calculate_times(file_name, __name__)
