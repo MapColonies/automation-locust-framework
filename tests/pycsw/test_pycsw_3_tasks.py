3736bb0c0bbf1ee02dcfcc0584e8e920c97a2615
shay-perpi <shayperp@rafael.co.il>
2023-06-05 10:33:42 +0300

    pycsw fix 1.0 by shay

--- tests/pycsw/test_pycsw_3_tasks.py
+++ tests/pycsw/test_pycsw_3_tasks.py
@@ -14,49 +14,75 @@ from locust import (
     constant_throughput,
     task,
 )
-from test_data.queries import ID_RECORD_XML, POLYGON_XML, REGION_RECORD_XML
+from utils.percentile_calculation import generate_name, calculate_times
+from test_data.queries import QUERY_TEMPLATE
+
+polygon = {'name': config_obj["pycsw"].PYCSW_POLYGON_PROPERTY, 'value': config_obj["pycsw"].PYCSW_POLYGON_VALUE}
+by_id = {'name': config_obj["pycsw"].PYCSW_ID_PROPERTY, 'value': config_obj["pycsw"].PYCSW_ID_VALUE}
+by_region = {'name': config_obj["pycsw"].PYCSW_REGION_PROPERTY, 'value': config_obj["pycsw"].PYCSW_REGION_VALUE}
+
+file_name = generate_name(__name__)
+stat_file = open(f"{config_obj['wmts'].root_dir}/{file_name}", 'w')
 
 
 class SizingUser(HttpUser):
-    timer_selection = config_obj["default"].WAIT_TIME_FUNC
-    wait_time = config_obj["default"].WAIT_TIME
-    if timer_selection == 1:
-        wait_time = constant(wait_time)
-        print(CONSTANT_TIMER_STR)
-    elif timer_selection == 2:
-        wait_time = constant_throughput(wait_time)
-        print(CONSTANT_THROUGHPUT_TIMER_STR)
-    elif timer_selection == 3:
-        wait_time = between(
-            config_obj["default"].MIN_WAIT, config_obj["default"].MAX_WAIT
-        )
-        print(BETWEEN_TIMER_STR)
-    elif timer_selection == 4:
-        wait_time = constant_pacing(wait_time)
-        print(CONSTANT_PACING_TIMER_STR)
-    else:
-        print(INVALID_TIMER_STR)
+    between(1, 1)
 
     @task(1)
     def get_records_by_polygon(self):
-        self.client.post(
-            "/",
-            data=POLYGON_XML.encode("UTF-8"),
-            verify=False,
-        )
+        if config_obj["pycsw"].TOKEN_FLAG:
+            self.client.post(
+                f"?token={config_obj['pycsw'].Token}",
+                data=QUERY_TEMPLATE(polygon).encode("UTF-8"),
+                verify=False,
+            )
+        else:
+            self.client.post(
+                "/",
+                data=QUERY_TEMPLATE(polygon).encode("utf-8"),
+                verify=False,
+            )
 
     @task(1)
     def get_records_by_id(self):
-        self.client.post(
-            "/",
-            data=ID_RECORD_XML.encode("utf-8"),
-            verify=False,
-        )
+        if config_obj["pycsw"].TOKEN_FLAG:
+            self.client.post(
+                f"?token={config_obj['pycsw'].Token}",
+                data=QUERY_TEMPLATE(by_id).encode("utf-8"),
+                verify=False,
+            )
+        else:
+            self.client.post(
+                "/",
+                data=QUERY_TEMPLATE(by_id).encode("utf-8"),
+                verify=False,
+            )
 
     @task(1)
     def get_records_by_region(self):
-        self.client.post(
-            "/",
-            data=REGION_RECORD_XML.encode("utf-8"),
-            verify=False,
-        )
+        if config_obj["pycsw"].TOKEN_FLAG:
+            self.client.post(
+                f"?token={config_obj['pycsw'].Token}",
+                data=QUERY_TEMPLATE(by_region).encode("utf-8"),
+                verify=False,
+            )
+            self.client.post(
+                "/",
+                data=QUERY_TEMPLATE(by_region).encode("utf-8"),
+                verify=False,
+            )
+
+    def on_stop(self):
+        calculate_times(file_name, __name__)
+
+
+# out of the class in the bottom add 2 listeners
+
+@events.request.add_listener
+def hook_request_success(request_type, name, response_time, response_length, **kwargs):
+    stat_file.write(f"{request_type};{name} ; {response_time};{response_length}  \n")
+
+
+@events.quitting.add_listener
+def hook_quitting(environment, **kw):
+    stat_file.close()
