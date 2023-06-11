e7946752c1d4aee63adae5428e2e738e5f2f5d5c
shay-perpi <shayperp@rafael.co.il>
2023-06-11 13:43:00 +0300

    adding upscale attributes, http2, wms with upsacle

---
+++ tests/wmts/one_by_one.py
@@ -0,0 +1,16 @@
+from config.config import config_obj
+from locust import between, task, FastHttpUser, tag
+from locust_plugins.csvreader import CSVReader
+
+ssn_reader = CSVReader(config_obj["wmts"].ONE_BY_ONE_RECORDS)
+
+
+class User(FastHttpUser):
+    between(1, 1)
+    host = config_obj["wmts"].HOST
+
+    @tag("OneByOneRequest")
+    @task(1)
+    def index(self):
+        url = next(ssn_reader)
+        self.client.get(url=url[0], verify=False)
