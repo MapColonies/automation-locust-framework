82e943561ea2c8fd6f6b9e7a1a6bce65d9f2d449
shayavr <shayavr@rafael.co.il>
2023-07-04 22:27:46 +0300

    add one point test

---
+++ tests/elevation_service/one_point_per_user.py
@@ -0,0 +1,92 @@
+import json
+import os
+from locust import HttpUser, events, task
+
+from common.config.config import ElevationConfig
+from common.validation.validation_utils import (
+    write_rps_percent_results, get_request_parameters, extract_points_from_json,
+)
+
+results_path = os.getcwd()
+positions_list_path = ElevationConfig.positions_path
+
+
+if type(ElevationConfig.percent_ranges_counters) == str:
+    percent_ranges = json.loads(ElevationConfig.percent_ranges_counters)
+else:
+    percent_ranges = ElevationConfig.percent_ranges_counters
+
+# request_body = get_request_parameters(positions_list_path=positions_list_path)
+
+positions_bodies = extract_points_from_json(json_file=positions_list_path)
+
+
+class CustomUser(HttpUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request_bodies = positions_bodies
+
+    def on_start(self):
+        index = self.environment.runner.user_count - 1
+        self.body = self.request_bodies[index % len(self.request_bodies)]
+
+    @task(1)
+    def index(self):
+        body = self.body
+        # if self.request_bodies["request_type"] == "json":
+        self.client.post(
+            "/", json=self.body, headers={'Content-Type': 'application/json'}
+        )
+        # elif self.request_bodies["request_type"] == "bin":
+        #     self.client.post(
+        #         "/",
+        #         data=self.request_bodies["body"],
+        #         headers=self.request_bodies["header"],
+        #     )
+
+        # Process the response as needed
+
+    def on_stop(self):
+        # Calculate and present the percentage results
+        percent_value_by_range = {}
+        for k in counters:
+            percent_range = (counters[k] / total_requests) * 100
+            percent_value_by_range[f"{k}"] = percent_range
+
+        percent_value_by_range["total_requests"] = total_requests
+        write_rps_percent_results(
+            custom_path=results_path, percente_value_by_range=percent_value_by_range
+        )
+
+
+counters = percent_ranges
+
+total_requests = 0
+
+
+@events.request.add_listener
+def response_time_listener(response_time, **kwargs):
+    global counters, total_requests
+
+    for k in counters:
+        if k[1] is None and response_time >= k[0]:
+            counters[k] = counters.get(k, 0) + 1
+        elif k[0] <= response_time <= k[1]:
+            counters[k] = counters.get(k, 0) + 1
+
+    total_requests += 1
+
+
+@events.test_start.add_listener
+def reset_counters(**kwargs):
+    global counters, total_requests
+    counters = percent_ranges
+    total_requests = 0
+
+
+# Run the Locust test
+class MyUser(CustomUser):
+    min_wait = 100
+    max_wait = 1000
+# todo:ask alex which wait time to set to insure that we create the next task only if we get reponse from the first task
+#todo: check the ranges validation - json.decoder.JSONDecodeError: Expecting property name enclosed in double quotes: line 1 column 2 (char 1)
