1bfa0e20e2b37c657d3d2ea87f97f441e6b72f80
shayavr <shayavr@rafael.co.il>
2023-07-07 13:35:29 +0300

    finish one point graph creation

--- tests/elevation_service/graph_function.py
+++ tests/elevation_service/graph_function.py
@@ -22,7 +22,6 @@ from locust.runners import STATE_STOPPED, STATE_CLEANUP, MasterRunner, LocalRunn
 
 # Print the last user count after the test execution
 
-
 class MyUser(HttpUser):
     wait_time = between(1, 2)
     test_results = []
@@ -31,42 +30,43 @@ class MyUser(HttpUser):
         super().__init__(*args, **kwargs)
         self.total_requests = 0
         self.total_response_time = 0
+        self.user_count = None
 
     @task
     def my_task(self):
         # Make the HTTP request and perform task logic
-        response = self.client.get("https://www.ynet.co.il/home")  # Replace with your actual task logic
+        self.client.get("https://www.ynet.co.il/home")  # Replace with your actual task logic
+
+        self.user_count = self.environment.runner.user_count
 
         # Do something with the response if needed
-        response_time = response.elapsed.total_seconds() * 1000
+        # response_time = response.elapsed.total_seconds() * 1000
 
-        self.total_requests += 1
-        self.total_response_time += response_time
+        # self.total_requests += 1
+        # self.total_response_time += response_time
 
     def on_stop(self, **kwargs):
-        if self.environment.runner.stats in [STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP]:
-            user_count = self.environment.runner.user_count
-
-            print("users------------>", user_count)
-        else:
-            user_count = self.environment.runner.user_count
-
-            print("users------------>", user_count)
         average_response_time = self.environment.runner.stats.total.avg_response_time
-        # print("average_response_time is", average_response_time)
-        # self.test_results.append({"users": users
-        #                              , "avg_response_time": average_response_time})
-        # self.plot_graph()
+        self.test_results.append({"users": self.user_count
+                                     , "avg_response_time": average_response_time})
+        self.plot_graph()
 
     def plot_graph(self):
         users = [result["users"] for result in self.test_results]
         avg_response_times = [result["avg_response_time"] for result in self.test_results]
 
-        plt.plot(users, avg_response_times, marker='o')
+        plt.plot( users,avg_response_times, marker='o')
+        plt.ylabel('Average Response Time')
         plt.xlabel('Number of Users')
-        plt.ylabel('Average Response Time (ms)')
-        plt.title('Test Set: Users vs Average Response Time')
+        plt.title("User amount vs Average Response Time")
         plt.grid(True)
+        # plt.plot(avg_response_times, users, marker='o')
+        # plt.xlabel('Average Response Time (ms)')
+        # plt.ylabel('User Count')
+        # plt.title('User Count vs. Average Response Time')
         plt.savefig('graph.png')
 
-
+# @events.quitting.add_listener
+# def on_locust_quit(environment, **kwargs):
+#     total_users = environment.runner.user_count
+#     print(f"Total users at the end of the test: {total_users}")
--- tests/elevation_service/one_point_per_user.py
+++ tests/elevation_service/one_point_per_user.py
@@ -1,12 +1,7 @@
 import json
-import os
-import time
-
 from locust import HttpUser, events, task, constant
 from itertools import cycle
-
 from matplotlib import pyplot as plt
-
 from common.config.config import ElevationConfig
 from common.validation.validation_utils import (
     write_rps_percent_results, extract_points_from_json, initiate_counters_by_ranges,
@@ -21,78 +16,59 @@ else:
 
 positions_bodies = extract_points_from_json(json_file=positions_list_path)
 
+reports_path = ElevationConfig.results_path
+
 
 class CustomUser(HttpUser):
-
-        # self.test_start_time = None
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.test_results = []
         self.request_bodies = positions_bodies
         self.request_bodies_cycle = cycle(self.request_bodies)
-        self.test_start_time = time.time()
-
-
+        self.graph_name = ElevationConfig.graph_name
+        self.users_count = None
 
     @task(1)
     def index(self):
-        # num_users = self.environment.runner.user_count
-        # print(f"Number of users: {num_users}")
-        body = next(self.request_bodies_cycle)
-        self.body = json.loads(body)
+        body = json.loads(next(self.request_bodies_cycle))
         # if self.request_bodies["request_type"] == "json":
-        self.client.post("/", json=self.body, headers={'Content-Type': 'application/json'}, verify=True)
+        self.client.post("/", json=body, headers={'Content-Type': 'application/json'}, verify=False)
         # elif self.request_bodies["request_type"] == "bin":
         #     self.client.post(
         #         "/",
         #         data=self.request_bodies["body"],
         #         headers=self.request_bodies["header"],
         #     )
-
-        # Process the response as needed
+        self.users_count = self.environment.runner.user_count
 
     def on_stop(self):
-        users = self.environment.runner.user_count
-        print(users)
-        # Calculate the test duration
-        test_duration = time.time() - self.test_start_time
-        print("test_duration is", test_duration)
+        average_response_time = self.environment.runner.stats.total.avg_response_time
+        self.test_results.append({"users": self.users_count, "avg_response_time": average_response_time})
+        self.plot_graph(graph_name=self.graph_name, graph_path=reports_path)
 
-        # Calculate the average RPS
-        avg_rps = total_requests / test_duration if test_duration else 0
+        percent_value_by_range = {}
+        if total_requests != 0:
+            for index, (key, value) in enumerate(counters.items()):
+                percent_range = (value / total_requests) * 100
+                percent_value_by_range[f"{percent_ranges[index]}"] = percent_range
+            percent_value_by_range["total_requests"] = total_requests
+            write_rps_percent_results(
+                custom_path=reports_path, percente_value_by_range=percent_value_by_range
+            )
+        else:
+            percent_value_by_range["total_requests"] = total_requests
 
-        print("avg_rps is", avg_rps)
-
-        # Store the data in the test_results list
-        self.test_results.append({"users": users, "rps": avg_rps})
-
-        # Plot the graph after the final test execution
-        # if self.environment.runner.user_count == 0:
-        self.plot_graph()
-        # percent_value_by_range = {}
-        # if total_requests != 0:
-        #     for index, (key, value) in enumerate(counters.items()):
-        #         percent_range = (value / total_requests) * 100
-        #         percent_value_by_range[f"{percent_ranges[index]}"] = percent_range
-        #     percent_value_by_range["total_requests"] = total_requests
-        #     write_rps_percent_results(
-        #         custom_path=ElevationConfig.results_path, percente_value_by_range=percent_value_by_range
-        #     )
-        # else:
-        #     percent_value_by_range["total_requests"] = total_requests
-
-    def plot_graph(self):
-        users = [result["users"] for result in self.test_results]
-        rps = [result["rps"] for result in self.test_results]
-
+    def plot_graph(self, graph_name, graph_path):
         # Plotting the graph
-        plt.plot(users, avg_total_requests, marker='o')
+        users = [result["users"] for result in self.test_results]
+        avg_response_times = [result["avg_response_time"] for result in self.test_results]
+
+        plt.plot(users, avg_response_times, marker='o')
+        plt.ylabel('Average Response Time')
         plt.xlabel('Number of Users')
-        plt.ylabel('Average Total Requests')
-        plt.title('Test Set: Users vs Average Total Requests')
+        plt.title("User amount vs Average Response Time")
         plt.grid(True)
-        plt.savefig('graph.png')
-        # plt.show()
+        plt.savefig(f'{graph_path}/{graph_name}.png')
 
 
 counters = initiate_counters_by_ranges(config_ranges=percent_ranges)
