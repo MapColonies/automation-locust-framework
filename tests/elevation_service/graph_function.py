a0b9ea38287328bc3968c199ef1d653d72746a62
shayavr <shayavr@rafael.co.il>
2023-07-09 18:15:20 +0300

    add User amount vs Average Response Time graph and start creating the generic function for the graph creation

--- tests/elevation_service/graph_function.py
+++ tests/elevation_service/graph_function.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from locust import task, events, HttpUser, between
@@ -6,6 +7,9 @@ import matplotlib.pyplot as plt
 import gevent
 from locust import events
 from locust.runners import STATE_STOPPED, STATE_CLEANUP, MasterRunner, LocalRunner, STATE_STOPPING
+
+from common.config.config import ElevationConfig
+from common.validation.validation_utils import create_custom_graph
 
 # def print_current_users(environment):
 #     while not environment.runner.state in [STATE_STOPPED, STATE_CLEANUP]:
@@ -17,7 +21,7 @@ from locust.runners import STATE_STOPPED, STATE_CLEANUP, MasterRunner, LocalRunn
 #     # don't run this on workers, we only care about the aggregated numbers
 #     if isinstance(environment.runner, MasterRunner) or isinstance(environment.runner, LocalRunner):
 #         gevent.spawn(print_current_users, environment)
-
+reports_path = ElevationConfig.results_path
 
 # Print the last user count after the test execution
 total_requests = 0
@@ -57,20 +61,45 @@ class MyUser(HttpUser):
         print("rps is", self.environment.runner.stats.total.total_rps)
 
     def plot_graph(self):
-        users = [result["users"] for result in self.test_results]
-        avg_response_times = [result["avg_response_time"] for result in self.test_results]
+        create_custom_graph(graph_name="Users_vs_AvgResponseTime", graph_path=reports_path,
+                            test_results=self.test_results, graph_title=None)
 
-        plt.plot(users, avg_response_times, marker='o')
-        plt.ylabel('Average Response Time')
-        plt.xlabel('Number of Users')
-        plt.title("User amount vs Average Response Time")
-        plt.grid(True)
-        # plt.plot(avg_response_times, users, marker='o')
-        # plt.xlabel('Average Response Time (ms)')
-        # plt.ylabel('User Count')
-        # plt.title('User Count vs. Average Response Time')
-        plt.savefig('graph.png')
+        create_custom_graph(graph_name='RequestStartTime_vs_ResponseTime',graph_path=reports_path,test_results=)
+        #todo think how to deal with the Request Start Time vs. Response Time graph for the generic function of creating custom graph
+
+        # users = [result["users"] for result in self.test_results]
+        # avg_response_times = [result["avg_response_time"] for result in self.test_results]
+        #
+        # plt.plot(users, avg_response_times, marker='o')
+        # plt.ylabel('Average Response Time')
+        # plt.xlabel('Number of Users')
+        # plt.title("User amount vs Average Response Time")
+        # plt.grid(True)
+        # # plt.plot(avg_response_times, users, marker='o')
+        # # plt.xlabel('Average Response Time (ms)')
+        # # plt.ylabel('User Count')
+        # # plt.title('User Count vs. Average Response Time')
+        # plt.savefig('graph.png')
+        # plt.close()
+
+        plt.scatter(start_time_data, response_time_data)
+        plt.xlabel('Request Start Time')
+        plt.ylabel('Response Time (ms)')
+        plt.title('Request Start Time vs. Response Time')
+        plt.savefig('POINTS.png')
         plt.close()
+
+
+start_time_data = []
+response_time_data = []
+
+
+@events.request.add_listener
+def on_request(request_type, name, response_time, **kwargs):
+    start_time = kwargs['start_time']
+    start_time_formatted = datetime.datetime.fromtimestamp(start_time).strftime('%H:%M:%S.%f')
+    start_time_data.append(start_time_formatted)
+    response_time_data.append(response_time)
 
 
 @events.request.add_listener
@@ -80,14 +109,3 @@ def on_request_success(request_type, name, response_time, response_length, **kwa
 
     total_requests += 1
     total_time += response_time
-    avg_requests_per_second = total_requests / total_time
-
-# @events.quitting.add_listener
-# def on_locust_quit(environment, **kwargs):
-#     avg_requests_per_second = total_requests / total_time
-#     print(f"Average Requests/s: {avg_requests_per_second}")
-
-# @events.quitting.add_listener
-# def on_locust_quit(environment, **kwargs):
-#     total_users = environment.runner.user_count
-#     print(f"Total users at the end of the test: {total_users}")
