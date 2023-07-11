8c42250bb794ea744c8fd71b911ca8d9a6c53d43
shayavr <shayavr@rafael.co.il>
2023-07-11 14:46:32 +0300

    finish the one point graph creation tests

--- tests/elevation_service/one_point_per_user.py
+++ tests/elevation_service/one_point_per_user.py
@@ -5,7 +5,8 @@ from itertools import cycle
 from matplotlib import pyplot as plt
 from common.config.config import ElevationConfig
 from common.validation.validation_utils import (
-    write_rps_percent_results, extract_points_from_json, initiate_counters_by_ranges,
+    write_rps_percent_results, extract_points_from_json, initiate_counters_by_ranges, create_custom_graph,
+    create_graph_results_data_format, create_start_time_response_time_graph,
 )
 
 positions_list_path = ElevationConfig.positions_path
@@ -28,6 +29,7 @@ class CustomUser(HttpUser):
         self.request_bodies_cycle = cycle(self.request_bodies)
         self.graph_name = ElevationConfig.graph_name
         self.users_count = None
+        self.req_start_t_rsp_t = []
 
     @task(1)
     def index(self):
@@ -45,45 +47,42 @@ class CustomUser(HttpUser):
     def on_stop(self):
         average_response_time = self.environment.runner.stats.total.avg_response_time
         self.test_results.append({"users": self.users_count, "avg_response_time": average_response_time})
-        self.plot_graph(graph_name=self.graph_name, graph_path=reports_path)
+        # print(self.test_results)
+        create_custom_graph(graph_name="Users_vs_AvgResponseTime", graph_path=reports_path,
+                            test_results=self.test_results, graph_title=None, rotation=None)
 
+        # self.req_start_t_rsp_t = create_graph_results_data_format(["start_time", "response_time"],
+        #                                                           [])
+        # print(self.req_start_t_rsp_t)
+        create_start_time_response_time_graph(graph_name=f"RequestStartTime_vs_ResponseTime-{run_number}",
+                                              start_time_data=start_time_data, response_time_data=response_time_data)
 
-        percent_value_by_range = {}
-        if total_requests != 0:
-            for index, (key, value) in enumerate(counters.items()):
-                percent_range = (value / total_requests) * 100
-                percent_value_by_range[f"{percent_ranges[index]}"] = percent_range
-            percent_value_by_range["total_requests"] = total_requests
-            write_rps_percent_results(
-                custom_path=reports_path, percente_value_by_range=percent_value_by_range
-            )
-        else:
-            percent_value_by_range["total_requests"] = total_requests
-
-    def plot_graph(self, graph_name, graph_path):
-        users = [result["users"] for result in self.test_results]
-        avg_response_times = [result["avg_response_time"] for result in self.test_results]
-        plt.plot(users, avg_response_times, marker='o')
-        plt.ylabel('Average Response Time')
-        plt.xlabel('Number of Users')
-        plt.title("User amount vs Average Response Time")
-        plt.grid(True)
-        plt.savefig(f'{graph_path}/{graph_name}.png')
+        # percent_value_by_range = {}
+        # if total_requests != 0:
+        #     for index, (key, value) in enumerate(counters.items()):
+        #         range_percent_val = (value / total_requests) * 100
+        #         percent_value_by_range[f"{percent_ranges[index]}"] = range_percent_val
+        #     percent_value_by_range["total_requests"] = total_requests
+        #     write_rps_percent_results(
+        #         custom_path=reports_path, percente_value_by_range=percent_value_by_range
+        #     )
+        # else:
+        #     percent_value_by_range["total_requests"] = total_requests
 
 
 counters = initiate_counters_by_ranges(config_ranges=percent_ranges)
 total_requests = 0
-
+run_number = 0
 start_time_data = []
 response_time_data = []
+file_created = False
 
 
 @events.request.add_listener
 def on_request(response_time, **kwargs):
-    start_time = kwargs['start_time']
-    start_time_formatted = datetime.datetime.fromtimestamp(start_time).strftime('%H:%M:%S.%f')
-    start_time_data.append(start_time_formatted)
+    start_time = datetime.datetime.fromtimestamp(kwargs['start_time'])
     response_time_data.append(response_time)
+    start_time_data.append(start_time)
 
 
 @events.request.add_listener
@@ -101,11 +100,32 @@ def response_time_listener(response_time, **kwargs):
 
 @events.test_start.add_listener
 def reset_counters(**kwargs):
-    global counters, total_requests
+    global counters, total_requests, run_number, start_time_data, response_time_data
     counters = counters
     total_requests = 0
+    run_number += 1
+    start_time_data = []
+    response_time_data = []
 
 
 # Run the Locust test
 class MyUser(CustomUser):
     wait_time = constant(ElevationConfig.wait_time)
+
+@events.test_stop.add_listener
+def on_locust_stop(environment, **kwargs):
+    global file_created
+
+    # Ensure file creation code is executed only once
+    if not file_created:
+        percent_value_by_range = {}
+        if total_requests != 0:
+            for index, (key, value) in enumerate(counters.items()):
+                range_percent_val = (value / total_requests) * 100
+                percent_value_by_range[f"{percent_ranges[index]}"] = range_percent_val
+            percent_value_by_range["total_requests"] = total_requests
+            write_rps_percent_results(
+                custom_path=reports_path, percente_value_by_range=percent_value_by_range
+            )
+        else:
+            percent_value_by_range["total_requests"] = total_requests
