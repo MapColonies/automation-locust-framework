a6a29863f560e9387bb3a5ff36b23dd22facc1a9
shayavr <shayavr@rafael.co.il>
2023-07-17 15:30:32 +0300

    fix graphs function and percent calculation

--- tests/elevation_service/one_point_per_user.py
+++ tests/elevation_service/one_point_per_user.py
@@ -24,12 +24,11 @@ reports_path = ElevationConfig.results_path
 class CustomUser(HttpUser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.test_results = []
         self.request_bodies = positions_bodies
         self.request_bodies_cycle = cycle(self.request_bodies)
         self.graph_name = ElevationConfig.graph_name
-        self.users_count = None
-        self.req_start_t_rsp_t = []
+
+
 
     @task(1)
     def index(self):
@@ -42,15 +41,12 @@ class CustomUser(HttpUser):
         #         data=self.request_bodies["body"],
         #         headers=self.request_bodies["header"],
         #     )
-        self.users_count = self.environment.runner.user_count
-
     def on_stop(self):
-
         average_response_time = self.environment.runner.stats.total.avg_response_time
-        self.test_results.append({"users": self.users_count, "avg_response_time": average_response_time})
+        # self.test_results.append({"users": self.users_count, "avg_response_time": average_response_time})
         # print(self.test_results)
-        create_custom_graph(graph_name="Users_vs_AvgResponseTime", graph_path=reports_path,
-                            test_results=self.test_results, graph_title=None)
+        # create_custom_graph(graph_name="Users_vs_AvgResponseTime", graph_path=reports_path,
+        #                     test_results=self.test_results, graph_title=None)
 
         # self.req_start_t_rsp_t = create_graph_results_data_format(["start_time", "response_time"],
         #                                                           [])
@@ -58,45 +54,24 @@ class CustomUser(HttpUser):
         create_start_time_response_time_graph(graph_name=f"RequestStartTime_vs_ResponseTime-{run_number}",
                                               start_time_data=start_time_data, response_time_data=response_time_data)
 
-        # percent_value_by_range = {}
-        # if total_requests != 0:
-        #     for index, (key, value) in enumerate(counters.items()):
-        #         range_percent_val = (value / total_requests) * 100
-        #         percent_value_by_range[f"{percent_ranges[index]}"] = range_percent_val
-        #     percent_value_by_range["total_requests"] = total_requests
-        #     write_rps_percent_results(
-        #         custom_path=reports_path, percente_value_by_range=percent_value_by_range
-        #     )
-        # else:
-        #     percent_value_by_range["total_requests"] = total_requests
 
-
-counters = initiate_counters_by_ranges(config_ranges=percent_ranges)
+# counters = initiate_counters_by_ranges(config_ranges=percent_ranges)
 total_requests = 0
-run_number = 0
+run_number = 1
 start_time_data = []
 response_time_data = []
-file_created = False
+users_count = 0
+test_results = []
 
 
 @events.request.add_listener
-def on_request(response_time, **kwargs):
+def on_request(environment, response_time, **kwargs):
+    global total_requests, users_count
     start_time = datetime.datetime.fromtimestamp(kwargs['start_time'])
     response_time_data.append(response_time)
     start_time_data.append(start_time)
-
-
-# @events.request.add_listener
-# def response_time_listener(response_time, **kwargs):
-#     global counters, total_requests
-#
-#     for index, range_val in enumerate(percent_ranges):
-#         if range_val[1] is None and response_time >= range_val[0]:
-#             counters[f"counter{index + 1}"] += 1
-#         elif range_val[0] <= response_time <= range_val[1]:
-#             counters[f"counter{index + 1}"] += 1
-#
-#     total_requests += 1
+    total_requests += 1
+    users_count = environment.runner.user_count
 
 
 @events.test_start.add_listener
@@ -109,35 +84,21 @@ def reset_counters(**kwargs):
     response_time_data = []
 
 
-# Run the Locust test
-class MyUser(CustomUser):
-    wait_time = constant(ElevationConfig.wait_time)
-    # wait_time = constant("5")
-
-
 @events.test_stop.add_listener
 def on_locust_stop(environment, **kwargs):
-    global file_created
-    percent_value_by_ranges = calculate_response_time_percent(response_times=response_time_data, range_values=percent_ranges)
+    average_response_time = environment.runner.stats.total.avg_response_time
+    test_results.append({"users": users_count, "avg_response_time": average_response_time})
+    print(test_results)
+    create_custom_graph(graph_name="Users_vs_AvgResponseTime", graph_path=reports_path,
+                        test_results=test_results, graph_title=None)
+    percent_value_by_ranges = calculate_response_time_percent(response_times=response_time_data,
+                                                              range_values=percent_ranges)
+    percent_value_by_ranges["total_requests"] = total_requests
     write_rps_percent_results(
-                custom_path=reports_path, percent_value_by_range=percent_value_by_ranges
-            )
+        custom_path=reports_path, percent_value_by_range=percent_value_by_ranges
+    )
 
-    # Ensure file creation code is executed only once
-    # if not file_created:
-    #     percent_value_by_range = {}
-    #     print(total_requests)
-    #     if total_requests != 0:
-    #         for index, (key, value) in enumerate(counters.items()):
-    #             print("index", index)
-    #             print("key", key)
-    #             print("value", value)
-    #             range_percent_val = (value / total_requests) * 100
-    #             percent_value_by_range[f"{percent_ranges[index]}"] = range_percent_val
-    #         percent_value_by_range["total_requests"] = total_requests
-    #         write_rps_percent_results(
-    #             custom_path=reports_path, percent_value_by_range=percent_value_by_range
-    #         )
-    #     else:
-    #         percent_value_by_range["total_requests"] = total_requests
 
+# Run the Locust test
+class MyUser(CustomUser):
+    wait_time = constant(int(ElevationConfig.wait_time))
