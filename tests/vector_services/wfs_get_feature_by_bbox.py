6f2cf1b293d16266fc487a4acba78d909204967b
SHYE AVRAHAM <shye.avraham1@gmail.com>
2025-05-13 18:34:49 +0300

    add polygon generator

--- tests/vector_services/wfs_get_feature_by_bbox.py
+++ tests/vector_services/wfs_get_feature_by_bbox.py
@@ -1,23 +1,28 @@
+import json
 import random
+
 from locust import FastHttpUser, task, constant
 from common.config.config import config_obj
 from common.utils.wfs_wrapper import WFSClient, GeoGenerator
 
-geo_generator = GeoGenerator(ROI_PATH=config_obj["wfs"].ROI_PATH)
+with open(config_obj["wfs"].BBOX_MAPPING, "r") as f:
+    bbox_mappings = json.load(f)
 
 
-class GetFeatureByIdUser(FastHttpUser):
+class GetFeatureByBboxUser(FastHttpUser):
     wait_time = constant(config_obj["wfs"].WAIT_TIME)
 
-    def on_start(self):
-        # Assign each user a random ID from the list
-        self.bbox = geo_generator.get_random_bbox()
-
     @task
-    def get_feature_by_id(self):
+    def get_feature_by_bbox(self):
+        mapping = random.choice(bbox_mappings)
+        geo_generator = GeoGenerator(ROI_PATH=mapping.get("roiPath"))
+        bbox = geo_generator.get_random_bbox(min_width=config_obj["wfs"].MIN_WIDTH,
+                                             max_width=config_obj["wfs"].MAX_WIDTH,
+                                             min_height=config_obj["wfs"].MIN_HEIGHT,
+                                             max_height=config_obj["wfs"].MAX_HEIGHT)
         my_client = WFSClient(base_url=config_obj["wfs"].WFS_URL, version=
         config_obj["wfs"].VERSION,
                               token=config_obj["wfs"].TOKEN)
         my_client.session = self.client
-        my_client.get_feature(request_params={"bbox": self.bbox,
-                                              "typeNames": config_obj["wfs"].TYPE_NAMES})
+        my_client.get_feature(request_params={"bbox": bbox,
+                                              "typeNames": mapping.get("typeName")})
