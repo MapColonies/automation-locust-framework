5a175cc509a00479dc3eaeb379603d52b3582d5b
shayavr <shayavr@rafael.co.il>
2025-07-13 19:18:59 +0300

    add logs to requests body

--- tests/vector_services/wfs_get_feature_by_point.py
+++ tests/vector_services/wfs_get_feature_by_point.py
@@ -1,9 +1,11 @@
+import logging
 import random
 from locust import FastHttpUser, task, constant
 from common.config.config import config_obj
 from common.utils.wfs_wrapper import WFSClient, GeoGenerator
 
 geo_generator = GeoGenerator(ROI_PATH=config_obj["wfs"].ROI_PATH)
+logger = logging.getLogger("locust.user")
 
 
 class GetFeatureByPointUser(FastHttpUser):
@@ -19,7 +21,7 @@ class GetFeatureByPointUser(FastHttpUser):
                               token=config_obj["wfs"].TOKEN)
 
         point_filter = my_client.create_wfs_point_dwithin_filter_body(point=point, distance=radius)
+        logger.info(f"Point filter: {point_filter}")
         my_client.session = self.client
         my_client.get_feature(filters=point_filter)
-
         my_client.session = self.client
