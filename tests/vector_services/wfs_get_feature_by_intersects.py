081a885f958dd38d772f07fa711d5e7278c9a5bd
shayavr <shayavr@rafael.co.il>
2025-07-13 19:18:59 +0300

    add logs to requests body

--- tests/vector_services/wfs_get_feature_by_intersects.py
+++ tests/vector_services/wfs_get_feature_by_intersects.py
@@ -1,10 +1,13 @@
 import json
+import logging
 import random
 
 from locust import FastHttpUser, task, constant
 from common.config.config import config_obj
 from common.utils.wfs_wrapper import WFSClient, GeoGenerator
 
+
+logger = logging.getLogger("locust.user")
 
 with open(config_obj["wfs"].BBOX_MAPPING, "r") as f:
     bbox_mappings = json.load(f)
@@ -17,13 +20,13 @@ class GetFeatureByIntersectsUser(FastHttpUser):
         mapping = random.choice(bbox_mappings)
         geo_generator = GeoGenerator(ROI_PATH=mapping.get("roiPath"))
         polygon = geo_generator.generate_random_polygon(vertex_count=5)
-        print(polygon)
+        logger.info(f"Generated polygon: {polygon}")
         my_client = WFSClient(base_url=config_obj["wfs"].WFS_URL, version=
         config_obj["wfs"].VERSION,
                               token=config_obj["wfs"].TOKEN)
 
         intersect_filter = my_client.create_intersects_filter(
             type_name=mapping.get("typeName"), pos_list=polygon)
-        print(intersect_filter)
+        logger.info(f"intersect_filter: {intersect_filter}")
         my_client.session = self.client
         my_client.get_feature(filters=intersect_filter)
