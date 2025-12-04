5d7f8274a2a2bdf93a83f95b87e62cb3d466012d
tamarshelly2 <tamars2@rafael.co.il>
2025-12-04 17:40:29 +0200

    add cms test

---
+++ tests/DEM/wcs_get_coverage.py
@@ -0,0 +1,33 @@
+from locust import HttpUser, task
+from requests.adapters import HTTPAdapter
+from common.config.config import config_obj
+from common.utils.wcs_client import WCSClient
+
+class NoNormHTTPAdapter(HTTPAdapter):
+    def request_url(self, request, proxies):
+        return request.url # Return exactly what the user gave – no normalization
+
+class WCSGetCoverageUser(HttpUser):
+    host = config_obj["wcs"].BASE_URL
+
+    def on_start(self):
+        self.wcs_client = WCSClient(
+            base_url=config_obj["wcs"].BASE_URL,
+            version=config_obj["wcs"].VERSION,
+            token=config_obj["wcs"].TOKEN
+        )
+        self.session = self.client #get to request.session()
+        self.session.mount("https://", NoNormHTTPAdapter())
+
+        self.coverage_id = config_obj["wcs"].COVERAGE_ID
+        self.layer_xml = self.wcs_client.get_describe_coverage(self.coverage_id, self.session)
+
+    @task
+    def get_coverage(self):
+        additional_params = config_obj["wcs"].RESOLUTIONS
+        # or: additional_params={"scaleSize": config_obj["wcs"].SCALE_SIZE}
+        # layer_xml = self.wcs_client.get_describe_coverage(self.coverage_id, self.session)
+        subsets = self.wcs_client.generate_subset_from_xml(self.layer_xml,config_obj["wcs"].BBOX_WIDTH, config_obj["wcs"].BBOX_HEIGHT)
+
+        res = self.wcs_client.get_coverage(self.coverage_id, config_obj["wcs"].FORMAT, subsets, additional_params,
+                                      session=self.session)
